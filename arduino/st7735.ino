d75e09e93e0808eef93dc6026a825fb757790f15
qitas <qitas@qitas.cn>
2019-07-29 22:29:17 +0800

    add

---
+++ arduino/st7735.ino
@@ -0,0 +1,31 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+
+//#define TFT_RST -1 //for TFT I2C Connector Shield V1.0.0 and TFT 1.4 Shield V1.0.0
+//#define TFT_CS D4  //for TFT I2C Connector Shield V1.0.0 and TFT 1.4 Shield V1.0.0
+//#define TFT_DC D3  //for TFT I2C Connector Shield V1.0.0 and TFT 1.4 Shield V1.0.0
+
+#define TFT_RST 16   //for TFT I2C Connector Shield (V1.1.0 or later)
+#define TFT_CS 4    //for TFT I2C Connector Shield (V1.1.0 or later)
+#define TFT_DC 5    //for TFT I2C Connector Shield (V1.1.0 or later)
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+void setup(void)
+{
+  tft.initR(INITR_144GREENTAB);
+  tft.setTextWrap(false); // Allow text to run off right edge
+  tft.fillScreen(ST7735_BLACK);
+}
+
+void loop(void)
+{
+  tft.fillScreen(ST7735_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST7735_BLUE);
+  tft.setTextSize(2);
+  tft.println("WEMOS.CC");
+  tft.setRotation(tft.getRotation() + 1);
+  delay(3000);
+}
